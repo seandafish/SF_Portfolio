16f2f4954a06374e28d7dd7d4663b4e2f520276e
Sean Fisher <83900293+seandafish@users.noreply.github.com>
2025-04-23 17:58:38 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle navigation menu on small screens
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
